e50e92dbcea40ef37e5154c731fdb06e7e99b339
Brook Elgie <specialbrew@gmail.com>
2018-10-16 14:36:11 +0100

    Fix up to work with ckanext toolkit

--- ckanext/contact/controllers/contact.py
+++ ckanext/contact/controllers/contact.py
@@ -1,31 +1,31 @@
 import logging
-import ckan.lib.base as base
 import ckan.plugins as p
 import ckan.logic as logic
 import ckan.model as model
 import ckan.lib.captcha as captcha
 import ckan.lib.navl.dictization_functions as dictization_functions
 import ckan.lib.mailer as mailer
-import ckan.lib.helpers as h
+
+import ckan.plugins.toolkit as toolkit
+
 import socket
 from pylons import config
-from ckan.common import _, request, c, response
 from ckanext.contact.interfaces import IContact
 
 log = logging.getLogger(__name__)
 
-render = base.render
-abort = base.abort
-redirect = base.redirect
+render = toolkit.render
+abort = toolkit.abort
 
 DataError = dictization_functions.DataError
 unflatten = dictization_functions.unflatten
 
-check_access = logic.check_access
-get_action = logic.get_action
+check_access = toolkit.check_access
+get_action = toolkit.get_action
 flatten_to_string_key = logic.flatten_to_string_key
 
-class ContactController(base.BaseController):
+
+class ContactController(toolkit.BaseController):
     """
     Controller for displaying a contact form
     """
@@ -35,24 +35,29 @@ class ContactController(base.BaseController):
         super(ContactController, self).__before__(action, **env)
 
         try:
-            self.context = {'model': model, 'session': model.Session, 'user': base.c.user or base.c.author, 'auth_user_obj': base.c.userobj}
+            self.context = {'model': model, 'session': model.Session,
+                            'user': toolkit.c.user or toolkit.c.author,
+                            'auth_user_obj': toolkit.c.userobj}
             check_access('send_contact', self.context)
         except logic.NotAuthorized:
-            base.abort(401, _('Not authorized to use contact form'))
+            abort(401, toolkit._('Not authorized to use contact form'))
 
     @staticmethod
     def _submit(context):
 
         try:
-            data_dict = logic.clean_dict(unflatten(logic.tuplize_dict(logic.parse_params(request.params))))
+            data_dict = \
+                logic.clean_dict(unflatten(logic.tuplize_dict(
+                                           logic.parse_params(
+                                            toolkit.request.params))))
             context['message'] = data_dict.get('log_message', '')
-            c.form = data_dict['name']
-            captcha.check_recaptcha(request)
+            toolkit.c.form = data_dict['name']
+            captcha.check_recaptcha(toolkit.request)
         except logic.NotAuthorized:
-            base.abort(401, _('Not authorized to see this page'))
+            abort(401, toolkit._('Not authorized to see this page'))
         except captcha.CaptchaError:
-            error_msg = _(u'Bad Captcha. Please try again.')
-            h.flash_error(error_msg)
+            error_msg = toolkit._(u'Bad Captcha. Please try again.')
+            toolkit.h.flash_error(error_msg)
 
         errors = {}
         error_summary = {}
@@ -72,11 +77,15 @@ class ContactController(base.BaseController):
         if len(errors) == 0:
 
             body = '%s' % data_dict["content"]
-            body += '\n\nSent by:\nName:%s\nEmail: %s\n' % (data_dict["name"], data_dict["email"])
+            body += '\n\nSent by:\nName:%s\nEmail: %s\n' % (data_dict["name"],
+                                                            data_dict["email"])
             mail_dict = {
-                'recipient_email': config.get("ckanext.contact.mail_to", config.get('email_to')),
-                'recipient_name': config.get("ckanext.contact.recipient_name", config.get('ckan.site_title')),
-                'subject': config.get("ckanext.contact.subject", 'Contact/Question from visitor'),
+                'recipient_email': config.get("ckanext.contact.mail_to",
+                                              config.get('email_to')),
+                'recipient_name': config.get("ckanext.contact.recipient_name",
+                                             config.get('ckan.site_title')),
+                'subject': config.get("ckanext.contact.subject",
+                                      'Contact/Question from visitor'),
                 'body': body,
                 'headers': {'reply-to': data_dict["email"]}
             }
@@ -88,10 +97,11 @@ class ContactController(base.BaseController):
             try:
                 mailer.mail_recipient(**mail_dict)
             except (mailer.MailerException, socket.error):
-                h.flash_error(_(u'Sorry, there was an error sending the email. Please try again later'))
+                toolkit.h.flash_error(
+                    toolkit._(u'Sorry, there was an error sending the email. Please try again later'))
             else:
                 data_dict['success'] = True
-                
+
         return data_dict, errors, error_summary
 
     def ajax_submit(self):
@@ -100,9 +110,11 @@ class ContactController(base.BaseController):
         @return:
         """
         data, errors, error_summary = self._submit(self.context)
-        data = flatten_to_string_key({'data': data, 'errors': errors, 'error_summary': error_summary})
-        response.headers['Content-Type'] = 'application/json;charset=utf-8'
-        return h.json.dumps(data)
+        data = flatten_to_string_key({'data': data, 'errors': errors,
+                                      'error_summary': error_summary})
+        toolkit.response.headers['Content-Type'] = \
+            'application/json;charset=utf-8'
+        return toolkit.h.json.dumps(data)
 
     def form(self):
 
@@ -116,18 +128,20 @@ class ContactController(base.BaseController):
         error_summary = {}
 
         # Submit the data
-        if 'save' in request.params:
+        if 'save' in toolkit.request.params:
             data, errors, error_summary = self._submit(self.context)
         else:
             # Try and use logged in user values for default values
             try:
-                data['name'] = base.c.userobj.fullname or base.c.userobj.name
-                data['email'] = base.c.userobj.email
+                data['name'] = \
+                    toolkit.c.userobj.fullname or toolkit.c.userobj.name
+                data['email'] = toolkit.c.userobj.email
             except AttributeError:
                 data['name'] = data['email'] = None
 
         if data.get('success', False):
             return p.toolkit.render('contact/success.html')
         else:
-            vars = {'data': data, 'errors': errors, 'error_summary': error_summary}
+            vars = {'data': data, 'errors': errors,
+                    'error_summary': error_summary}
             return p.toolkit.render('contact/form.html', extra_vars=vars)
